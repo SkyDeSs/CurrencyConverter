b11aae5b44736aee2b66c97d0683aee7b78f935b
SkyDeSs <denisenko3333@gmail.com>
2022-11-17 23:59:41 +0200

    added main script

---
+++ script.js
@@ -0,0 +1,18 @@
+const input = document.querySelector("#first_value"),
+      output = document.querySelector("#second_value");
+
+input.addEventListener('input', () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", 'currencies.json');
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send();
+
+  request.addEventListener('load', () => {
+    if (request.status === 200) {
+      const data = JSON.parse(request.response);
+      output.value = (+input.value / data.currency.usd).toFixed(2);
+    } else {
+        alert("Error occured");
+    }
+  });
+});
