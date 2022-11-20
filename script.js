e3e9907badcd35fdec3dd66b2b4a75f6e582b47b
SkyDeSs <denisenko3333@gmail.com>
2022-11-20 16:26:15 +0200

    added feature to script.js

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ input.addEventListener('input', () => {
     }
   });
 });
+
+output.addEventListener('input', () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", 'currencies.json');
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send();
+
+  request.addEventListener('load', () => {
+    if (request.status === 200) {
+      const data = JSON.parse(request.response);
+      input.value = (+output.value * data.currency.usd).toFixed(2);
+    } else {
+        alert("Error occured");
+    }
+  });
+});
+
