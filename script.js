2a9a78a735fe98b051a296721ced9ff29389afa1
Denys Zemliakov <denisenko3333@gmail.com>
2022-11-26 23:47:07 +0200

    added currency list, connected to external currency rates (json), edited css

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 const input = document.querySelector("#first_value"),
-      output = document.querySelector("#second_value");
+  output = document.querySelector("#second_value"),
+  currencySelector = document.querySelector("#currency_select");
 
-input.addEventListener('input', () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", 'currencies.json');
-  request.setRequestHeader("Content-Type", "application/json");
-  request.send();
+function currenciesList() {
+  const currenciesRequest = new XMLHttpRequest();
 
-  request.addEventListener('load', () => {
-    if (request.status === 200) {
-      const data = JSON.parse(request.response);
-      output.value = (+input.value / data.currency.usd).toFixed(2);
+  currenciesRequest.open("GET", "http://www.floatrates.com/daily/uah.json");
+  currenciesRequest.setRequestHeader("Content-Type", "application/json");
+  currenciesRequest.send();
+
+  currenciesRequest.addEventListener("load", () => {
+    if (currenciesRequest.status === 200) {
+      const currenciesObj = JSON.parse(currenciesRequest.response);
+      const iterate = (obj) => {
+        Object.keys(obj).forEach((key) => {
+          if (key == "name") {
+            currencySelector.innerHTML += `<option value=${obj["code"]}> ${obj[key]} </option>`;
+          } else if (typeof obj[key] === "object" && obj[key] !== null) {
+            iterate(obj[key]);
+          }
+        });
+      };
+      iterate(currenciesObj);
     } else {
-        alert("Error occured");
+      alert("Error at loading currencies");
     }
   });
-});
+}
 
-output.addEventListener('input', () => {
-  const request = new XMLHttpRequest();
-  request.open("GET", 'currencies.json');
-  request.setRequestHeader("Content-Type", "application/json");
-  request.send();
+currenciesList();
 
-  request.addEventListener('load', () => {
-    if (request.status === 200) {
-      const data = JSON.parse(request.response);
-      input.value = (+output.value * data.currency.usd).toFixed(2);
+input.addEventListener("input", convertCurrencies, false);
+currencySelector.addEventListener("change", convertCurrencies, false);
+
+
+function convertCurrencies() { 
+  const convertionRequest = new XMLHttpRequest();
+  convertionRequest.open("GET", "http://www.floatrates.com/daily/uah.json");
+  convertionRequest.setRequestHeader("Content-Type", "application/json");
+  convertionRequest.send();
+
+  convertionRequest.addEventListener("load", () => {
+    if (convertionRequest.status === 200 && currencySelector.value !== null) {
+      const data = JSON.parse(convertionRequest.response);
+      output.value = (
+        +input.value * data[currencySelector.value.toLowerCase()].inverseRate
+      ).toFixed(2);
     } else {
-        alert("Error occured");
+      alert("Error occured");
     }
   });
-});
+};
 
